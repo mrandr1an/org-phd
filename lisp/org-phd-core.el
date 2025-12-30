2db19b08f5be9d9c8696ba8700022d4c7971962f
Chris Liourtas <krackedissad@gmail.com>
2025-12-30 19:29:08 +0200

    experiment: Attempt 1 to see if multi-file setup works.

---
+++ lisp/org-phd-core.el
@@ -0,0 +1,6 @@
+(defun hello-world ()
+  "Test function."
+  (message "hello world"))
+
+(provide 'org-phd-core)
+;;; org-phd-core.el ends here
--- org-phd.el
+++ org-phd.el
@@ -5,15 +5,26 @@
 ;;; Commentary:
 ;;; Code:
 
+(eval-and-compile
+  (add-to-list 'load-path
+               (expand-file-name "lisp" (file-name-directory (or load-file-name buffer-file-name)))))
+
 ;;;###autoload
 (define-minor-mode org-phd-minor-mode
   "Test description."
   :init-value nil
   :lighter " org-phd"
   (if org-phd-minor-mode
-      (message "org-phd minor mode enabled")
-    (message "org-phd minor mode disabled")))
+      (progn
+	(require 'org-phd-core)
+	(message "org-phd minor mode enabled"))
+    (message "org-phd minor mode disabled"))
+ )
 
 (provide 'org-phd)
 ;;; org-phd.el ends here
 
+(use-package org-phd
+  :elpaca (:host github :repo "mrandr1an/org-phd")
+  :commands (org-phd-minor-mode)
+)
