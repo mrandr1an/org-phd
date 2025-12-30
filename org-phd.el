dd644003c92275c7e784380d2a73a4534673c13e
Chris Liourtas <krackedissad@gmail.com>
2025-12-30 19:06:52 +0200

    Initial Commit

---
+++ org-phd.el
@@ -0,0 +1,15 @@
+;;; org-phd --- org-phd.el
+;; Version: 0.0.1
+;; Package-Requires: ((emacs "30.2"))
+;; Keywords: Convenience, Project Management, Writting
+;;; Commentary:
+;;; Code:
+
+(define-minor-mode org-phd-minor-mode
+  "Test description."
+  :init-value nil
+  (message "org-phd-mode enabled")
+)
+
+(provide 'org-phd)
+;;; org-phd.el ends here
