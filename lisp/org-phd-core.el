0a696db33987a5d61d0d336865410bf333ec273c
Chris Liourtas <krackedissad@gmail.com>
2025-12-30 20:45:23 +0200

    experiment: Try basic org reading

--- lisp/org-phd-core.el
+++ lisp/org-phd-core.el
@@ -1,7 +1,20 @@
-(defun org-phd-hello-world ()
+(defgroup org-phd nil
+  "Group for org-phd."
+  :group 'org
+)
+
+(defcustom org-phd-types '(publication note blog agenda)
+  "Types of org-phd content."
+  :type '(repeat symbol)
+  :group 'org-phd
+)
+
+(defun org-phd-read-org ()
   "Test function."
   (interactive)
-  (message "hello world"))
+  (let ((key (org-collect-keywords '("TITLE"))))
+    (message "%S" key))
+)
 
 (provide 'org-phd-core)
 ;;; org-phd-core.el ends here
